b52b3664a10eec1c626500dd952c39f13e485d6a
VenkataKrishna <24071a1273@vnrvjiet.in>
2026-04-10 21:24:31 +0530

    Update DB connection logic

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase= async ()=>{
     try{
-        await mongoose.connect(process.env.MONGODB_URL)
+        await mongoose.connect(process.env.MONGODB_URL) 
     }catch(err){
         console.log(err);
     }
